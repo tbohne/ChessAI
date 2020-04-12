263d991f9de9a758d29443f96f198cf4d019d726
tbohne <tbohne@uos.de>
2020-04-12 14:04:16 +0200

    implemented basic browser UI

--- chess_ai.py
+++ chess_ai.py
@@ -1,5 +1,12 @@
 import chess
 import random
+from flask import Flask, Response, request
+import chess.svg
+import time
+import base64
+
+
+app = Flask(__name__)
 
 
 def random_move():
@@ -8,35 +15,41 @@ def random_move():
     board.push_uci(str(move))
 
 
-def visualize_board():
-    print("#####################")
-    print("#####################")
-    print(board)
-    print("#####################")
-    print("#####################")
+def to_svg(board):
+    return base64.b64encode(chess.svg.board(board = board).encode('utf-8')).decode('utf-8')
 
 
-def run_game():
-    while not board.is_game_over():
-        while True:
+@app.route("/")
+def root():
+    board_svg = to_svg(board)
+    html = '<html><head></head><body>'
+    html += '<html><body><img width=680 src="data:image/svg+xml;base64,%s"></img>' % board_svg
+    html += '<form action="/move"><input name="move" type="text"></input><input type="submit" value="Move"></form><br/>'
+    return html
+
+
+@app.route("/move")
+def move():
+    if not board.is_game_over():
+        move = request.args.get('move', default = "")
+        human_move_performed = False
+
+        if move is not None and move != "":
+            print("move:", move)
             try:
-                move = input()
                 board.push_uci(move)
-                visualize_board()
-                if board.is_game_over():
-                    print("PLAYER WINS")
-                break
+                human_move_performed = True
             except:
                 print("invalid move..", move)
                 if (move == "q"):
                     exit(1)
-        random_move()
-        if board.is_game_over():
-            print("AI WINS")
-        visualize_board()
+
+        if human_move_performed:
+            random_move()
+
+    return root()
 
 
 if __name__ == '__main__':
     board = chess.Board()
-    visualize_board()
-    run_game()
+    app.run(debug = True)
