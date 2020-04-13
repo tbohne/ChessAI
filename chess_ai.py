51ff3868c61f738a6da19d26ccd8b5dac1f956b1
tbohne <tbohne@uos.de>
2020-04-13 15:16:35 +0200

    implemented basic user feedback (hint texts)

--- chess_ai.py
+++ chess_ai.py
@@ -20,16 +20,20 @@ def to_svg(board):
 
 
 @app.route("/")
-def root():
+def root(hint_text = ""):
     board_svg = to_svg(board)
     html = '<html><head></head><body style="background-color: #6e6862; text-align: center">'
     html += '<html><body><img width=680 src="data:image/svg+xml;base64,%s"></img>' % board_svg
     html += '<form action="/move"><input name="move" type="text"></input><br/><input type="submit" value="Move"></form><br/>'
+    html += '<p style="color: #F5F5DC;"><b>%s</b></p>' % hint_text
     return html
 
 
 @app.route("/move")
 def move():
+
+    hint_text = ""
+
     if not board.is_game_over():
         move = request.args.get('move', default = "")
         human_move_performed = False
@@ -40,14 +44,19 @@ def move():
                 board.push_uci(move)
                 human_move_performed = True
             except:
-                print("invalid move..", move)
+                hint_text = "invalid move: " + move
+                print(hint_text)
                 if (move == "q"):
                     exit(1)
 
-        if human_move_performed:
+        if human_move_performed and board.is_game_over():
+            hint_text = "HUMAN WINS"
+        elif human_move_performed:
             random_move()
+            if board.is_game_over():
+                hint_text = "AI WINS"
 
-    return root()
+    return root(hint_text)
 
 
 if __name__ == '__main__':
