3911d2e4882f81f30bfd346fbc71e8902edc5695
tbohne <tbohne@uni-osnabrueck.de>
2020-08-21 10:35:31 +0200

    implemented basic random valid move AI opponent

--- chess_ai.py
+++ chess_ai.py
@@ -5,60 +5,63 @@ import chess.svg
 import time
 import base64
 
-
 app = Flask(__name__)
 
-
-def random_move():
-    legal_moves = [move for move in board.legal_moves]
-    move = random.choice(legal_moves)
-    board.push_uci(str(move))
-
-
-def to_svg(board):
-    return base64.b64encode(chess.svg.board(board = board).encode('utf-8')).decode('utf-8')
-
+def get_board_svg(board):
+    """Returns an SVG representation of the current board state."""
+    return base64.b64encode(chess.svg.board(board=board).encode('utf-8')).decode('utf-8')
 
 @app.route("/")
-def root(hint_text = ""):
-    board_svg = to_svg(board)
+def root(hint_text_ai="", hint_text_human="", hint_text_general=""):
     html = '<html><head></head><body style="background-color: #6e6862; text-align: center">'
-    html += '<html><body><img width=680 src="data:image/svg+xml;base64,%s"></img>' % board_svg
-    html += '<form action="/move"><input name="move" type="text"></input><br/><input type="submit" value="Move"></form><br/>'
-    html += '<p style="color: #F5F5DC;"><b>%s</b></p>' % hint_text
+    html += '<img width=680 src="data:image/svg+xml;base64,%s"></img>' % get_board_svg(board)
+    html += '<form action="/move"><input name="move" type="text"></input><br/><input type="submit" value="Move"></input></form>'
+    html += '<p style="color: #F5F5DC;"><b>%s</b></p>' % hint_text_ai
+    html += '<p style="color: #F5F5DC;"><b>%s</b></p>' % hint_text_human
+    html += '<p style="color: #F5F5DC;"><b>%s</b></p>' % hint_text_general
+    html += '</body></html>'
     return html
 
+def get_random_move():
+    """Returns a random move from the set of legal moves."""
+    legal_moves = [move for move in board.legal_moves]
+    move = random.choice(legal_moves)
+    return str(move)
 
 @app.route("/move")
 def move():
 
-    hint_text = ""
+    hint_text_ai = ""
+    hint_text_human = ""
+    hint_text_general = ""
 
     if not board.is_game_over():
-        move = request.args.get('move', default = "")
+
+        move = request.args.get('move', default="")
         human_move_performed = False
 
         if move is not None and move != "":
-            print("move:", move)
+            hint_text_human = "human move: " + move
             try:
                 board.push_uci(move)
                 human_move_performed = True
-            except:
-                hint_text = "invalid move: " + move
-                print(hint_text)
+            except Exception as e:
+                print(e)
+                hint_text_human = "invalid move: " + move
                 if (move == "q"):
                     exit(1)
 
         if human_move_performed and board.is_game_over():
-            hint_text = "HUMAN WINS"
+            hint_text_general = "HUMAN WINS"
         elif human_move_performed:
-            random_move()
+            move = get_random_move()
+            hint_text_ai = "computer move: " + move
+            board.push_uci(move)
             if board.is_game_over():
-                hint_text = "AI WINS"
+                hint_text_general = "AI WINS"
 
-    return root(hint_text)
-
+    return root(hint_text_ai, hint_text_human, hint_text_general)
 
 if __name__ == '__main__':
     board = chess.Board()
-    app.run(debug = True)
+    app.run(debug=True)
