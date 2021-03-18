d02a7cfcd68d1d6baaf0d78fb8ca04cf8f632fcd
tbohne <tbohne@uni-osnabrueck.de>
2021-03-18 17:32:35 +0100

    fix structure

--- README.md
+++ README.md
@@ -8,3 +8,11 @@ Approach to build a program that beats me in the game of chess.
 ### DEPENDENCIES
 - [python-chess](https://python-chess.readthedocs.io/en/latest/) - move generation, move validation and support for common formats
 - [flask](https://flask.palletsprojects.com/en/1.1.x/) - lightweight WSGI web application framework
+
+### 1st approach: [Minimax](https://en.wikipedia.org/wiki/Minimax)
+- minimizing the possible loss for a worst case (maximum loss) scenario
+- reasonably works up to a search depth of *3*
+- already surprinsingly good opponent, but beatable
+- **improvement**
+    - [alpha–beta pruning](https://en.wikipedia.org/wiki/Alpha%E2%80%93beta_pruning)
+    - decrease the number of nodes that are evaluated by the minimax algorithm 
--- minimax.py
+++ minimax.py
@@ -3,13 +3,15 @@ import eval
 import chess
 
 
-def minimax_step(depth: int, maximizing: bool, board: chess.Board) -> float:
+def minimax_step(depth: int, maximizing: bool, board: chess.Board, alpha: int, beta: int) -> float:
     """Performs a step in the minimax algorithm.
 
     Args:
         depth:      current depth in minimax tree
         maximizing: whether the current step is a maximizing step
         board:      board state to perform minimax for
+        alpha:      best value the maximizer can guarantee at the current level or above
+        beta:       best value the minimizer can guarantee at the current level or above
 
     Returns:
         minimax value
@@ -22,15 +24,21 @@ def minimax_step(depth: int, maximizing: bool, board: chess.Board) -> float:
         max_val = float('-inf')
         for move in board.legal_moves:
             board.push_uci(str(move))
-            max_val = max(max_val, minimax_step(depth - 1, not maximizing, board))
+            max_val = max(max_val, minimax_step(depth - 1, not maximizing, board, alpha, beta))
+            alpha = max(alpha, max_val)
             board.pop()
+            if beta <= alpha:
+                break
         return max_val
 
     min_val = float('inf')
     for move in board.legal_moves:
         board.push_uci(str(move))
-        min_val = min(min_val, minimax_step(depth - 1, not maximizing, board))
+        min_val = min(min_val, minimax_step(depth - 1, not maximizing, board, alpha, beta))
+        beta = min(beta, min_val)
         board.pop()
+        if beta <= alpha:
+            break
     return min_val
 
 
@@ -53,7 +61,7 @@ def minimax(depth: int, maximizing: bool, board: chess.Board) -> chess.Move:
 
     for move in board.legal_moves:
         board.push_uci(str(move))
-        val = min(best_val, minimax_step(depth - 1, not maximizing, board))
+        val = min(best_val, minimax_step(depth - 1, not maximizing, board, int('-inf'), int('inf')))
         board.pop()
         if val < best_val:
             best_val = val
