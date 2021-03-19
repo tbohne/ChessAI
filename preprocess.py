0f20266f59cf6af089b157f42b05834a1ac1b499
tbohne <tbohne@uni-osnabrueck.de>
2021-03-19 18:32:11 +0100

    hacky preprocessing / training approach

---
+++ preprocess.py
@@ -0,0 +1,116 @@
+import chess.pgn
+import numpy as np
+
+
+def make_matrix(board):
+    pgn = board.epd()
+    foo = []
+    pieces = pgn.split(" ", 1)[0]
+    rows = pieces.split("/")
+    for row in rows:
+        foo2 = []
+        for thing in row:
+            if thing.isdigit():
+                for i in range(0, int(thing)):
+                    foo2.append('.')
+            else:
+                foo2.append(thing)
+        foo.append(foo2)
+    return foo
+
+
+def translate(matrix, chess_dict):
+    rows = []
+    for row in matrix:
+        terms = []
+        for term in row:
+            terms.append(chess_dict[term])
+        rows.append(terms)
+    return rows
+
+
+chess_dict = {
+    'p': [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    'P': [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+    'n': [0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    'N': [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+    'b': [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    'B': [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
+    'r': [0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+    'R': [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+    'q': [0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+    'Q': [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0],
+    'k': [0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
+    'K': [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    '.': [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+}
+
+
+def value_func(res):
+    if res == "1/2-1/2":
+        return 0
+    elif res == "1-0":
+        return 1
+    elif res == "0-1":
+        return -1
+    else:
+        print("PROBLEM")
+        exit(1)
+
+
+# def fen_to_vec(fen):
+#     positions, move_right, castling, en_passant, half_moves, move_num = fen.split(" ")
+#     print(move_right)
+
+# def pass_board_to_vec(board, game):
+#     move_vec = []
+#     move_val = []
+#     for i, move in enumerate(game.mainline_moves()):
+#         board.push(move)
+#         value = game.headers["Result"]
+#         matrix = make_matrix(board.copy())
+#         rows = translate(matrix, chess_dict)
+#         move_vec.append([rows])
+#         move_val.append(value_func(value))
+#
+#     move_vec = np.array(move_vec)#.reshape(1, 8, 8, 12)
+#     move_val = np.array(move_val)
+#     return move_vec, move_val
+
+
+def get_training_data(num_of_examples):
+    pgn = open("data/training_games.pgn")
+    X = []
+    Y = []
+    game = chess.pgn.read_game(pgn)
+    cnt = 0
+
+    while game:
+        if cnt == num_of_examples:
+            break
+        board = game.board()
+        #vec, val = pass_board_to_vec(board, game)
+
+        for i, move in enumerate(game.mainline_moves()):
+            board.push(move)
+            value = game.headers["Result"]
+            matrix = make_matrix(board.copy())
+            rows = translate(matrix, chess_dict)
+            X.append(rows)
+            Y.append(value_func(value))
+
+        game = chess.pgn.read_game(pgn)
+        cnt += 1
+
+    X = np.array(X)
+    Y = np.array(Y)
+    return X, Y
+
+
+if __name__ == '__main__':
+
+    X, Y = get_training_data(500)
+    np.savez("data/dataset.npz", X, Y)
+
+
+
---
+++ train.py
@@ -0,0 +1,112 @@
+from torch.utils.data import Dataset
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class ChessValueDataset(Dataset):
+
+    def __init__(self):
+        dat = np.load("data/dataset.npz", allow_pickle=True)
+        self.X = dat['arr_0']
+        self.Y = dat['arr_1']
+
+    def __len__(self):
+        return self.X.shape[0]
+
+    def __getitem__(self, idx):
+        return (self.X[idx], self.Y[idx])
+
+class Net(nn.Module):
+
+    def __init__(self):
+        super(Net, self).__init__()
+        self.a1 = nn.Conv2d(8, 16, kernel_size=3)
+        self.a2 = nn.Conv2d(16, 16, kernel_size=3)
+        self.a3 = nn.Conv2d(16, 32, kernel_size=3)
+
+        self.b1 = nn.Conv2d(32, 32, kernel_size=3, padding=2)
+        self.b2 = nn.Conv2d(32, 32, kernel_size=3, padding=2)
+        self.b3 = nn.Conv2d(32, 64, kernel_size=3, padding=2)
+
+        self.c1 = nn.Conv2d(64, 64, kernel_size=2)
+        self.c2 = nn.Conv2d(64, 64, kernel_size=2)
+        self.c3 = nn.Conv2d(64, 128, kernel_size=2, padding=1)
+
+        self.d1 = nn.Conv2d(128, 128, kernel_size=2)
+        self.d2 = nn.Conv2d(128, 128, kernel_size=2)
+        self.d3 = nn.Conv2d(128, 128, kernel_size=2)
+
+        self.last = nn.Linear(128, 1)
+
+    def forward(self, x):
+
+        x = F.relu(self.a1(x))
+        x = F.relu(self.a2(x))
+        x = F.relu(self.a3(x))
+        x = F.max_pool2d(x, 2)
+
+        # 4x4
+        x = F.relu(self.b1(x))
+        x = F.relu(self.b2(x))
+        x = F.relu(self.b3(x))
+        x = F.max_pool2d(x, 2)
+
+        # 2x2
+        x = F.relu(self.c1(x))
+        x = F.relu(self.c2(x))
+        x = F.relu(self.c3(x))
+        x = F.max_pool2d(x, 2)
+
+        # 1x128
+        x = x.view(-1, 128)
+        x = self.last(x)
+
+        # value oputput
+        return F.log_softmax(x)
+
+chess_dataset = ChessValueDataset()
+model = Net()
+num_epochs = 10
+device = 'cpu' #torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+loss_function = nn.CrossEntropyLoss()
+model.to(device)
+
+train_loader = torch.utils.data.DataLoader(chess_dataset, batch_size=256, shuffle=True)
+
+total_step = len(chess_dataset)
+for epoch in range(num_epochs):
+    for i, (input, target) in enumerate(train_loader):
+
+        # Move tensors to the configured device
+        # input = torch.cuda.FloatTensor(input)
+        # target = torch.cuda.FloatTensor(target)
+
+        #target = target.unsqueeze(-1)
+
+        #input = input.to(device)
+        target = target.to(device)
+
+        #input = torch.tensor(input, dtype=torch.long, device=device)
+
+        input = input.float()
+        target = target.float()
+
+        # Forward pass
+        outputs = model(input)
+
+        #outputs = outputs.reshape(len(outputs))
+
+        floss = nn.MSELoss()
+        loss = floss(outputs, target)
+
+        # Backward and optimize
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        print('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}'.format(epoch+1, num_epochs, i+1, total_step, loss.item()))
+
